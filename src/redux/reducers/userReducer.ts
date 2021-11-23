c12a7e0090318489537a12c9f5fc3dc1928675af
EduardoCitelli <eduardo.citelli@hotmail.com>
2021-11-23 01:38:42 -0300

    add user reducers

--- src/redux/reducers/userReducer.ts
+++ src/redux/reducers/userReducer.ts
@@ -1,21 +1,51 @@
 import { User } from "../../models/user";
-import { UserActionType, ALL_USERS_SUCCESS } from "../types/users/userTypes";
+import { UserActionType, ALL_USERS_SUCCESS, GET_USER_SUCCESS, CREATE_USER_SUCCESS, UPDATE_USER_SUCCESS, DELETE_USER_SUCCESS } from "../types/users/userTypes";
 
 
-interface UserState {
+interface UsersState {
     users: User[];
     error: string;
 }
 
-const defaultState: UserState = {
+interface UserState extends UsersState {
+    user: User;
+}
+
+const defaultState: UsersState = {
     users: [],
     error: '',
 }
 
-export default function userReducer(state = defaultState, action: UserActionType): UserState {
+export default function userReducer(state = defaultState, action: UserActionType): UsersState | UserState {
     switch (action.type) {
         case ALL_USERS_SUCCESS:
             return { users: action.users, error: '' }
+
+        case GET_USER_SUCCESS:
+            return {
+                ...state,
+                user: action.user
+            }
+
+        case CREATE_USER_SUCCESS:
+            return {
+                users: [...state.users, { ...action.user }],
+                error: ''
+            };
+
+        case UPDATE_USER_SUCCESS:
+            return {
+                users: state.users.map(user =>
+                    user.id === action.user.id ? action.user : user), error: ''
+            };
+
+        case DELETE_USER_SUCCESS:
+            return {
+                users: state.users.filter(user =>
+                    user.id !== action.user.id
+                ),
+                error: '',
+            }
 
         default:
             return state
